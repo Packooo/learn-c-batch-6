2939f600783039c80f59de4a03da9c96854bb82a
Packooo <you@example.com>
2023-01-14 15:51:16 +0700

    first commit

---
+++ Nestedloopingdowhile.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int tinggi, baris, kolom;
+	cout << "Tinggi segitiga : ";
+	cin >> tinggi;
+	cout << endl;
+	baris = 1;
+	do {
+		kolom = 1;
+		do {
+		cout <<'*';
+		kolom++;
+		} while (kolom <= baris);
+	cout << endl;
+	baris++;
+	} while (baris <= tinggi);
+}
